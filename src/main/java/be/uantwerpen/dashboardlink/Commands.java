dfca846f83eac0c3beeb6fc9198e922263104f3d
pixar02 <pixar.pf@gmail.com>
2023-04-01 16:01:05 +0200

    added reload and reset Commands

---
+++ src/main/java/be/uantwerpen/dashboardlink/Commands.java
@@ -0,0 +1,35 @@
+package be.uantwerpen.dashboardlink;
+
+import be.uantwerpen.dashboardlink.dashboard.Advancements;
+import be.uantwerpen.dashboardlink.dashboard.Dashboard;
+import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+
+import java.util.Collection;
+
+public class Commands {
+
+    public static void resetCommand(ServerCommandSource source, Collection<? extends Entity> targets){
+        for (Entity entity: targets) {
+            PlayerEntity player = (PlayerEntity) entity;
+            Dashboard.revokeAdvancements(player.getName().getString());
+        }
+
+        if (targets.size() == 1) {
+            source.sendFeedback(
+                    Text.literal("Reseting progress for " + targets.iterator().next().getDisplayName().getString()), true);
+        } else {
+            source.sendFeedback(
+                    Text.literal("Reseting progress for " + targets.size() + " players"), true);
+
+        }
+    }
+
+    public static void reloadCommand(ServerCommandSource source){
+        source.sendFeedback(Text.literal("Reloading Mapping"), true);
+        DashboardLink.advancements = new Advancements();
+    }
+}
--- src/main/java/be/uantwerpen/dashboardlink/event/ServerModEvents.java
+++ src/main/java/be/uantwerpen/dashboardlink/event/ServerModEvents.java
@@ -1,7 +1,20 @@
 package be.uantwerpen.dashboardlink.event;
 
+import be.uantwerpen.dashboardlink.Commands;
 import be.uantwerpen.dashboardlink.DashboardLink;
 import be.uantwerpen.dashboardlink.dashboard.Dashboard;
+import com.google.common.collect.ImmutableList;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+
+import java.util.Collection;
+
+import static net.minecraft.server.command.CommandManager.argument;
+import static net.minecraft.server.command.CommandManager.literal;
 
 public class ServerModEvents {
 
@@ -19,6 +32,22 @@ public class ServerModEvents {
                 }
                 Dashboard.updateAdvancement(DashboardLink.advancements.getAdvancementID(adv_name), player.getEntityName());
             }
+        });
+
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            dispatcher.register(literal("link").requires(source -> source.hasPermissionLevel(4))
+                    .then(literal("reset").then(argument("who", EntityArgumentType.entities()).executes(context -> {
+                        Commands.resetCommand(context.getSource(), EntityArgumentType.getEntities(context, "who"));
+                        return 1;
+                    })).executes(context -> { // No arugment (@a, @p)/
+                        Commands.resetCommand(context.getSource(), ImmutableList.of(context.getSource().getEntityOrThrow()));
+                        return 1;
+                    })
+            ).then(literal("reload") .executes(context -> {
+                //Register reload command
+                Commands.reloadCommand(context.getSource());
+                return 1;
+            })));
         });
     }
 }
